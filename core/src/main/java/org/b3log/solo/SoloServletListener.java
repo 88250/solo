e852ccf4f5b9849f1a283f65df7262fde1fac8f4
Liang Ding <DL88250@gmail.com>
2012-08-16 10:14:37 +0800

    版本 0.5.0

--- core/src/main/java/org/b3log/solo/SoloServletListener.java
+++ core/src/main/java/org/b3log/solo/SoloServletListener.java
@@ -52,7 +52,7 @@ import org.json.JSONObject;
  * B3log Solo servlet listener.
  *
  * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
- * @version 1.0.7.4, Jun 2, 2011
+ * @version 1.0.7.5, Aug 16, 2011
  * @since 0.3.1
  */
 public final class SoloServletListener extends AbstractServletListener {
@@ -60,7 +60,7 @@ public final class SoloServletListener extends AbstractServletListener {
     /**
      * B3log Solo version.
      */
-    public static final String VERSION = "0.4.6";
+    public static final String VERSION = "0.5.0";
     /**
      * Logger.
      */
