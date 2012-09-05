5221b91ba8c4869eb448abce6b0d007b0610f110
Vanessa <lly219@gmail.com>
2012-09-05 18:17:02 +0800

    c

--- war/src/main/webapp/js/tools/compress.js
+++ war/src/main/webapp/js/tools/compress.js
@@ -1,55 +1,5 @@
 var fs = require("fs"),
 exec = require("child_process").exec;
-/*
-// b3log js compress
-exec("uglifyjs ../../js/common.js > ../../js/common.min.js", function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log(error);
-        } 	
-});
-exec("uglifyjs ../../js/page.js > ../../js/page.min.js", function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log(error);
-        } 	
-});
-// combi admin js
-var adminJs = fs.readFileSync("../../js/admin/admin.js");
-adminJs += fs.readFileSync("../../js/admin/editor.js");
-adminJs += fs.readFileSync("../../js/admin/editorTinyMCE.js");
-adminJs += fs.readFileSync("../../js/admin/editorKindEditor.js");
-adminJs += fs.readFileSync("../../js/admin/editorCodeMirror.js");
-adminJs += fs.readFileSync("../../js/admin/tablePaginate.js");
-adminJs += fs.readFileSync("../../js/admin/article.js");
-adminJs += fs.readFileSync("../../js/admin/comment.js");
-adminJs += fs.readFileSync("../../js/admin/articleList.js");
-adminJs += fs.readFileSync("../../js/admin/draftList.js");
-adminJs += fs.readFileSync("../../js/admin/pageList.js");
-adminJs += fs.readFileSync("../../js/admin/others.js");
-adminJs += fs.readFileSync("../../js/admin/linkList.js");
-adminJs += fs.readFileSync("../../js/admin/preference.js");
-adminJs += fs.readFileSync("../../js/admin/pluginList.js");
-adminJs += fs.readFileSync("../../js/admin/userList.js");
-adminJs += fs.readFileSync("../../js/admin/commentList.js");
-adminJs += fs.readFileSync("../../js/admin/plugin.js");
-adminJs += fs.readFileSync("../../js/admin/main.js");
-adminJs += fs.readFileSync("../../js/admin/about.js");
-fs.writeFileSync("../../js/admin/latkeAdmin.js", adminJs, "UTF-8");
-// compress admin js
-exec("uglifyjs ../../js/admin/latkeAdmin.js > ../../js/admin/latkeAdmin.min.js", function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log(error);
-        } 	
-});
-
-// b3log css compress
-var b3logCsses = ["default-admin", "default-base", "default-init"];
-for (var i = 0; i < b3logCsses.length; i++) {
-    exec("lessc -compress ../../css/" + b3logCsses[i] + ".css > ../../css/" + b3logCsses[i] + ".min.css", function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log(error);
-        } 	
-    });
-}*/
 
 // skin js compress
 var jsPaths = ["ease"];
