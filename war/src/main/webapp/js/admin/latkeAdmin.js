1a2bbdf96d5c44eecad5127f1000afc50d0de02a
mainlove <wmainlove@gmail.com>
2013-01-25 15:10:19 +0800

    formatter

--- war/src/main/webapp/js/admin/latkeAdmin.js
+++ war/src/main/webapp/js/admin/latkeAdmin.js
@@ -3086,7 +3086,6 @@ admin.pluginList = {
             success: function(result, textStatus){
                 $("#tipMsg").text(result.msg);
                 
-<<<<<<< HEAD
 
                 $("#PluginSetting").html(result);
                 $("#PluginSetting").dialog({
@@ -3101,20 +3100,11 @@ admin.pluginList = {
                 $("#pluginSetting").html(result);
                 $("#pluginSetting").dialog("open");
                 
-<<<<<<< HEAD
 
 =======
 >>>>>>> branch '0.5.6' of https://github.com/b3log/b3log-solo.git
-                $("#loadMsg").text("");
-            }
-        });
+                $(
     },
-<<<<<<< HEAD
-
-=======
-    
- 
->>>>>>> branch '0.5.6' of https://github.com/b3log/b3log-solo.git
     changeStatus: function (pluginId, status) {
         if (status === "ENABLED") {
             status = "DISABLED";
