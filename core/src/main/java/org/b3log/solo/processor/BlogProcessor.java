3a3b812afc98ee4b9c84d2f3a624c0509acc8e1a
Liang Ding <DL88250@gmail.com>
2014-07-18 12:45:03 +0800

    Fix #11984

--- core/src/main/java/org/b3log/solo/processor/BlogProcessor.java
+++ core/src/main/java/org/b3log/solo/processor/BlogProcessor.java
@@ -17,18 +17,28 @@ package org.b3log.solo.processor;
 
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.b3log.latke.Keys;
 import org.b3log.latke.Latkes;
 import org.b3log.latke.RuntimeEnv;
+import org.b3log.latke.model.Pagination;
+import org.b3log.latke.model.User;
 import org.b3log.latke.servlet.HTTPRequestContext;
 import org.b3log.latke.servlet.HTTPRequestMethod;
 import org.b3log.latke.servlet.annotation.RequestProcessing;
 import org.b3log.latke.servlet.annotation.RequestProcessor;
 import org.b3log.latke.servlet.renderer.JSONRenderer;
+import org.b3log.latke.util.MD5;
+import org.b3log.latke.util.Strings;
 import org.b3log.solo.SoloServletListener;
+import org.b3log.solo.model.Article;
 import org.b3log.solo.model.Statistic;
 import org.b3log.solo.service.ArticleQueryService;
 import org.b3log.solo.service.StatisticQueryService;
 import org.b3log.solo.service.TagQueryService;
+import org.b3log.solo.service.UserQueryService;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
@@ -36,7 +46,7 @@ import org.json.JSONObject;
  * Blog processor.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.0.2, Jan 29, 2013
+ * @version 1.1.0.3, Jul 18, 2014
  * @since 0.4.6
  */
 @RequestProcessor
@@ -61,22 +71,28 @@ public class BlogProcessor {
     private StatisticQueryService statisticQueryService;
 
     /**
+     * User query service.
+     */
+    @Inject
+    private UserQueryService userQueryService;
+
+    /**
      * Gets blog information.
-     * 
+     *
      * <ul>
-     *   <li>Time of the recent updated article</li>
-     *   <li>Article count</li>
-     *   <li>Comment count</li>
-     *   <li>Tag count</li>
-     *   <li>Serve path</li>
-     *   <li>Static serve path</li>
-     *   <li>Solo version</li>
-     *   <li>Runtime environment (GAE/LOCAL)</li>
-     *   <li>Locale</li>
+     * <li>Time of the recent updated article</li>
+     * <li>Article count</li>
+     * <li>Comment count</li>
+     * <li>Tag count</li>
+     * <li>Serve path</li>
+     * <li>Static serve path</li>
+     * <li>Solo version</li>
+     * <li>Runtime environment (GAE/LOCAL)</li>
+     * <li>Locale</li>
      * </ul>
-     * 
+     *
      * @param context the specified context
-     * @throws Exception exception 
+     * @throws Exception exception
      */
     @RequestProcessing(value = "/blog/info", method = HTTPRequestMethod.GET)
     public void getBlogInfo(final HTTPRequestContext context) throws Exception {
@@ -104,6 +120,97 @@ public class BlogProcessor {
         jsonObject.put("runtimeEnv", runtimeEnv);
         if (RuntimeEnv.LOCAL == runtimeEnv) {
             jsonObject.put("runtimeDatabase", Latkes.getRuntimeDatabase());
+        }
+    }
+
+    /**
+     * Gets tags of all articles.
+     *
+     * <pre>
+     * {
+     *     "data": [
+     *         ["tag1", "tag2", ....], // tags of one article
+     *         ["tagX", "tagY", ....], // tags of another article
+     *         ....
+     *     ]
+     * }
+     * </pre>
+     *
+     * @param context the specified context
+     * @param request the specified HTTP servlet request
+     * @param response the specified HTTP servlet response
+     * @throws Exception io exception
+     */
+    @RequestProcessing(value = "/blog/articles-tags", method = HTTPRequestMethod.GET)
+    public void getArticlesTags(final HTTPRequestContext context, final HttpServletRequest request, final HttpServletResponse response)
+        throws Exception {
+        final String pwd = request.getParameter("pwd");
+
+        if (Strings.isEmptyOrNull(pwd)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+
+            return;
+        }
+
+        final JSONObject admin = userQueryService.getAdmin();
+
+        if (!MD5.hash(pwd).equals(admin.getString(User.USER_PASSWORD))) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+
+            return;
+        }
+
+        final JSONObject requestJSONObject = new JSONObject();
+
+        requestJSONObject.put(Pagination.PAGINATION_CURRENT_PAGE_NUM, 1);
+        requestJSONObject.put(Pagination.PAGINATION_PAGE_SIZE, Integer.MAX_VALUE);
+        requestJSONObject.put(Pagination.PAGINATION_WINDOW_SIZE, Integer.MAX_VALUE);
+        requestJSONObject.put(Article.ARTICLE_IS_PUBLISHED, true);
+
+        final JSONArray excludes = new JSONArray();
+
+        excludes.put(Article.ARTICLE_CONTENT);
+        excludes.put(Article.ARTICLE_UPDATE_DATE);
+        excludes.put(Article.ARTICLE_CREATE_DATE);
+        excludes.put(Article.ARTICLE_AUTHOR_EMAIL);
+        excludes.put(Article.ARTICLE_HAD_BEEN_PUBLISHED);
+        excludes.put(Article.ARTICLE_IS_PUBLISHED);
+        excludes.put(Article.ARTICLE_RANDOM_DOUBLE);
+
+        requestJSONObject.put(Keys.EXCLUDES, excludes);
+
+        final JSONObject result = articleQueryService.getArticles(requestJSONObject);
+        final JSONArray articles = result.optJSONArray(Article.ARTICLES);
+
+        final JSONRenderer renderer = new JSONRenderer();
+
+        context.setRenderer(renderer);
+
+        final JSONObject ret = new JSONObject();
+
+        renderer.setJSONObject(ret);
+
+        final JSONArray data = new JSONArray();
+
+        ret.put("data", data);
+
+        for (int i = 0; i < articles.length(); i++) {
+            final JSONObject article = articles.optJSONObject(i);
+            final String tagString = article.optString(Article.ARTICLE_TAGS_REF);
+
+            final JSONArray tagArray = new JSONArray();
+
+            data.put(tagArray);
+
+            final String[] tags = tagString.split(",");
+
+            for (final String tag : tags) {
+                final String trim = tag.trim();
+
+                if (!Strings.isEmptyOrNull(trim)) {
+                    tagArray.put(tag);
+                }
+            }
         }
     }
 }
