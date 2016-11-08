5af1821f0ad74270c9ae98945b03205ca59e5304
Liang Ding <DL88250@gmail.com>
2016-11-08 20:03:56 +0800

    重构 - 移除冗余依赖

--- src/test/java/org/b3log/solo/processor/MetaWeblogAPITestCase.java
+++ src/test/java/org/b3log/solo/processor/MetaWeblogAPITestCase.java
@@ -23,14 +23,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
-
 import javax.servlet.ReadListener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateUtils;
 import org.b3log.latke.model.User;
 import org.b3log.solo.AbstractTestCase;
@@ -40,16 +38,15 @@ import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 /**
  * {@link org.b3log.solo.api.metaweblog.MetaWeblogAPI} test case.
  *
  * @author yugt
  */
 @Test(suiteName = "api")
-public class MetaWeblogAPITestCase extends AbstractTestCase  {
+public class MetaWeblogAPITestCase extends AbstractTestCase {
 
-	/**
+    /**
      * Init.
      *
      * @throws Exception exception
@@ -71,83 +68,83 @@ public class MetaWeblogAPITestCase extends AbstractTestCase  {
 
     /**
      * 手动构造rpc请求
-     * 
+     *
      * @throws Exception exception
      */
     @Test(dependsOnMethods = "init")
     public void metaWeblog() throws Exception {
-    	final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HttpServletRequest request = mock(HttpServletRequest.class);
         when(request.getServletContext()).thenReturn(mock(ServletContext.class));
         when(request.getRequestURI()).thenReturn("/apis/metaweblog");
         when(request.getMethod()).thenReturn("POST");
-        
+
 //        Date date = (Date) DateFormatUtils.ISO_DATETIME_FORMAT.parseObject("2004-05-03T17:30:08");
         Date date = DateUtils.parseDate("20040503T17:30:08", new String[]{"yyyyMMdd'T'HH:mm:ss", "yyyyMMdd'T'HH:mm:ss'Z'"});
-        
-        
-		final class MockServletInputStream extends ServletInputStream {
-			private ByteArrayInputStream stream;
-			public MockServletInputStream(byte[] data) {
-				stream = new ByteArrayInputStream(data);
-			}
 
-			public int read() throws IOException {
-				return stream.read();
-			}
+        final class MockServletInputStream extends ServletInputStream {
 
-			@Override
-			public boolean isFinished() {
-				return false;
-			}
+            private ByteArrayInputStream stream;
 
-			@Override
-			public boolean isReady() {
-				return false;
-			}
+            public MockServletInputStream(byte[] data) {
+                stream = new ByteArrayInputStream(data);
+            }
 
-			@Override
-			public void setReadListener(ReadListener readListener) {
-			}
-		}
-		StringBuilder sb=new StringBuilder();
-		sb.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>")
-			.append("<methodCall>")
-				.append("<methodName>metaWeblog.newPost</methodName>")
-				.append("<params>")
-					.append("<param>")
-						.append("<value><int>11</int></value>")
-					.append("</param>")
-					.append("<param>")
-						.append("<value><string>test@gmail.com</string></value>")
-					.append("</param>")
-					.append("<param>")
-						.append("<value><string>pass</string></value>")
-					.append("</param>")
-					.append("<param>")
-						.append("<value>")
-							.append("<struct>")
-								.append("<member>")
-									.append("<name>dateCreated</name>")
-									.append("<value><dateTime.iso8601>20040503T17:30:08</dateTime.iso8601></value>")
-								.append("</member>")
-								.append("<member>")
-									.append("<name>title</name>")
-									.append("<value><string>title</string></value>")
-								.append("</member>")
-								.append("<member>")
-									.append("<name>description</name>")
-									.append("<value><string>description</string></value>")
-								.append("</member>")
-							.append("</struct>")
-						.append("</value>")
-					.append("</param>")
-					.append("<param>")
-						.append("<value><boolean>1</boolean></value>")
-					.append("</param>")
-				.append("</params>")
-			.append("</methodCall>");
+            public int read() throws IOException {
+                return stream.read();
+            }
+
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>")
+                .append("<methodCall>")
+                .append("<methodName>metaWeblog.newPost</methodName>")
+                .append("<params>")
+                .append("<param>")
+                .append("<value><int>11</int></value>")
+                .append("</param>")
+                .append("<param>")
+                .append("<value><string>test@gmail.com</string></value>")
+                .append("</param>")
+                .append("<param>")
+                .append("<value><string>pass</string></value>")
+                .append("</param>")
+                .append("<param>")
+                .append("<value>")
+                .append("<struct>")
+                .append("<member>")
+                .append("<name>dateCreated</name>")
+                .append("<value><dateTime.iso8601>20040503T17:30:08</dateTime.iso8601></value>")
+                .append("</member>")
+                .append("<member>")
+                .append("<name>title</name>")
+                .append("<value><string>title</string></value>")
+                .append("</member>")
+                .append("<member>")
+                .append("<name>description</name>")
+                .append("<value><string>description</string></value>")
+                .append("</member>")
+                .append("</struct>")
+                .append("</value>")
+                .append("</param>")
+                .append("<param>")
+                .append("<value><boolean>1</boolean></value>")
+                .append("</param>")
+                .append("</params>")
+                .append("</methodCall>");
         when(request.getInputStream()).thenReturn(new MockServletInputStream(sb.toString().getBytes()));
-        
 
         final MockDispatcherServlet dispatcherServlet = new MockDispatcherServlet();
         dispatcherServlet.init();
@@ -161,12 +158,13 @@ public class MetaWeblogAPITestCase extends AbstractTestCase  {
         dispatcherServlet.service(request, response);
 
         final String content = stringWriter.toString();
-        System.out.println("xxxxxcontent:"+content);
-//        Assert.assertTrue(StringUtils.startsWith(content, "{\"staticServePath\":\"http://localhost:8080\""));
+        System.out.println("xxxxxcontent:" + content);
+        Assert.assertTrue(StringUtils.startsWith(content, "<?xml version=\"1.0\""));
     }
-	
+
     /**
      * 使用XmlRpcClient发送rpc请求
+     *
      * @throws Exception exception
      */
 //    @Test(dependsOnMethods = "init")
@@ -190,18 +188,18 @@ public class MetaWeblogAPITestCase extends AbstractTestCase  {
 //        
 //        
 //    }
+    class Struct {
 
-    class Struct{
-    	String title="title";
-    	String link="link";
-    	String description="description";
-    	String author="author";
-    	String[] category={"category1","category2"};
-    	String comments="comments";
-    	String enclosure="enclosure";
-    	String guid="guid";
-    	String pubDate="pubDate";
-    	String source="source";
+        String title = "title";
+        String link = "link";
+        String description = "description";
+        String author = "author";
+        String[] category = {"category1", "category2"};
+        String comments = "comments";
+        String enclosure = "enclosure";
+        String guid = "guid";
+        String pubDate = "pubDate";
+        String source = "source";
     }
-    
+
 }
