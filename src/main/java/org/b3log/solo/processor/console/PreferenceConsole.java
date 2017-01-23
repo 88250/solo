611861c648140214fc617790e0d3231876bf297b
Liang Ding <dl88250@gmail.com>
2017-01-23 10:21:36 +0800

    :art: 更新文件头版权日期
    又过了一年，希望大家在新的一年里收获更多！
    有空多来社区逛逛 https://hacpai.com :sweat_smile:

--- src/main/java/org/b3log/solo/processor/console/PreferenceConsole.java
+++ src/main/java/org/b3log/solo/processor/console/PreferenceConsole.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2016, b3log.org & hacpai.com
+ * Copyright (c) 2010-2017, b3log.org & hacpai.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
