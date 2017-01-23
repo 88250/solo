55eff47e71ad964bb30fbdc9cca42fb65cc305e2
Liang Ding <dl88250@gmail.com>
2017-01-23 10:04:59 +0800

    :bookmark: 发布 1.8.0

--- src/main/java/org/b3log/solo/SoloServletListener.java
+++ src/main/java/org/b3log/solo/SoloServletListener.java
@@ -60,7 +60,7 @@ import org.json.JSONObject;
  * Solo Servlet listener.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.9.2.17, Nov 9, 2016
+ * @version 1.9.2.18, Jan 23, 2017
  * @since 0.3.1
  */
 public final class SoloServletListener extends AbstractServletListener {
@@ -68,7 +68,7 @@ public final class SoloServletListener extends AbstractServletListener {
     /**
      * Solo version.
      */
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.8.0";
 
     /**
      * Logger.
