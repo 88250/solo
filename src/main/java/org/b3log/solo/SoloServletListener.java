3ff4c5184fdf8b2761104962d6ea7bc9d5c8252f
Liang Ding <DL88250@gmail.com>
2017-04-10 22:09:22 +0800

    :wrench: 1.9.0 -> 2.0.0 升级程序

--- src/main/java/org/b3log/solo/SoloServletListener.java
+++ src/main/java/org/b3log/solo/SoloServletListener.java
@@ -61,7 +61,7 @@ import java.util.Set;
  * Solo Servlet listener.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.9.2.19, Feb 20, 2017
+ * @version 1.9.2.20, Apr 10, 2017
  * @since 0.3.1
  */
 public final class SoloServletListener extends AbstractServletListener {
@@ -69,7 +69,7 @@ public final class SoloServletListener extends AbstractServletListener {
     /**
      * Solo version.
      */
-    public static final String VERSION = "1.9.0";
+    public static final String VERSION = "2.0.0";
 
     /**
      * Logger.
