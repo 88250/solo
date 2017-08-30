6bcd7fe947c9130b39b4e32689767a46fabf23e1
Liang Ding <dl88250@gmail.com>
2017-08-30 13:52:38 +0800

    :zap: #12319  缓存统计数据

--- src/main/java/org/b3log/solo/repository/impl/StatisticRepositoryImpl.java
+++ src/main/java/org/b3log/solo/repository/impl/StatisticRepositoryImpl.java
@@ -15,18 +15,21 @@
  */
 package org.b3log.solo.repository.impl;
 
-
+import org.b3log.latke.Keys;
+import org.b3log.latke.ioc.inject.Inject;
 import org.b3log.latke.repository.AbstractRepository;
+import org.b3log.latke.repository.RepositoryException;
 import org.b3log.latke.repository.annotation.Repository;
+import org.b3log.solo.cache.StatisticCache;
 import org.b3log.solo.model.Statistic;
 import org.b3log.solo.repository.StatisticRepository;
-
+import org.json.JSONObject;
 
 /**
  * Statistic repository.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.0.2, May 15, 2013
+ * @version 1.1.0.0, Aug 30, 2017
  * @since 0.3.1
  */
 @Repository
@@ -37,5 +40,43 @@ public class StatisticRepositoryImpl extends AbstractRepository implements Stati
      */
     public StatisticRepositoryImpl() {
         super(Statistic.STATISTIC);
+    }
+
+    /**
+     * Statistic cache.
+     */
+    @Inject
+    private StatisticCache statisticCache;
+
+    @Override
+    public void remove(final String id) throws RepositoryException {
+        super.remove(id);
+
+        statisticCache.removeStatistic(id);
+    }
+
+    @Override
+    public JSONObject get(final String id) throws RepositoryException {
+        JSONObject ret = statisticCache.getStatistic(id);
+        if (null != ret) {
+            return ret;
+        }
+
+        ret = super.get(id);
+        if (null == ret) {
+            return null;
+        }
+
+        statisticCache.putStatistic(ret);
+
+        return ret;
+    }
+
+    @Override
+    public void update(final String id, final JSONObject statistic) throws RepositoryException {
+        super.update(id, statistic);
+
+        statistic.put(Keys.OBJECT_ID, id);
+        statisticCache.putStatistic(statistic);
     }
 }
