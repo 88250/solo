011bdf36e88cb09bc8f269a3cc3dbd4998f1a047
Liang Ding <DL88250@gmail.com>
2017-11-02 00:12:28 +0800

    :package: #12353

--- src/main/webapp/js/admin/latkeAdmin.js
+++ src/main/webapp/js/admin/latkeAdmin.js
@@ -1049,7 +1049,7 @@ $.extend(TablePaginate.prototype, {
  *
  * @author <a href="http://vanessa.b3log.org">Liyuan Li</a>
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.4.5.7, May 21, 2017
+ * @version 1.4.6.7, Oct 19, 2017
  */
 admin.article = {
     currentEditorType: '',
@@ -1495,7 +1495,7 @@ admin.article = {
                     height: 160,
                     buttonText: Label.selectLabel,
                     data: tags
-                }).width($("#tag").parent().width() - 68);
+                }).innerWidth($("#tag").parent().width() - 68);
 
                 $("#loadMsg").text("");
             }
@@ -2578,135 +2578,157 @@ admin.register["page-list"] = {
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
- * others for admin
+ * others for admin.
  *
  * @author <a href="http://vanessa.b3log.org">Liyuan Li</a>
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.0.8, Jul 27, 2016
+ * @version 1.2.0.0, Nov 1, 2017
  */
 
 /* oterhs 相关操作 */
 admin.others = {
-    /*
-     * @description 初始化
-     */
-    init: function () {
-        $("#tabOthers").tabs();
+  /*
+   * @description 初始化
+   */
+  init: function () {
+    $("#tabOthers").tabs();
 
-        $.ajax({
-            url: latkeConfig.servePath + "/console/reply/notification/template",
-            type: "GET",
-            cache: false,
-            success: function (result, textStatus) {
-                $("#tipMsg").text(result.msg);
-                if (!result.sc) {
-                    $("#loadMsg").text("");
-                    return;
-                }
+    $.ajax({
+      url: latkeConfig.servePath + "/console/reply/notification/template",
+      type: "GET",
+      cache: false,
+      success: function (result, textStatus) {
+        $("#tipMsg").text(result.msg);
+        if (!result.sc) {
+          $("#loadMsg").text("");
+          return;
+        }
 
-                $("#replayEmailTemplateTitle").val(result.replyNotificationTemplate.subject);
-                $("#replayEmailTemplateBody").val(result.replyNotificationTemplate.body);
+        $("#replayEmailTemplateTitle").val(result.replyNotificationTemplate.subject);
+        $("#replayEmailTemplateBody").val(result.replyNotificationTemplate.body);
 
-                $("#loadMsg").text("");
-            }
-        });
-    },
-    /*
-     * @description 移除未使用的标签。
-     */
-    removeUnusedTags: function () {
-        $("#tipMsg").text("");
+        $("#loadMsg").text("");
+      }
+    });
+  },
+  /*
+   * @description 移除未使用的标签
+   */
+  removeUnusedTags: function () {
+    $("#tipMsg").text("");
 
-        $.ajax({
-            url: latkeConfig.servePath + "/console/tag/unused",
-            type: "DELETE",
-            cache: false,
-            success: function (result, textStatus) {
-                $("#tipMsg").text(result.msg);
-            }
-        });
-    },
-    /*
-     * @description 移除未使用的标签。
-     */
-    exportSQL: function () {
-        $("#tipMsg").text("");
+    $.ajax({
+      url: latkeConfig.servePath + "/console/tag/unused",
+      type: "DELETE",
+      cache: false,
+      success: function (result, textStatus) {
+        $("#tipMsg").text(result.msg);
+      }
+    });
+  },
+  /*
+   * @description 导出数据为 SQL 文件
+   */
+  exportSQL: function () {
+    $("#tipMsg").text("");
 
-        $.ajax({
-            url: latkeConfig.servePath + "/console/export/sql",
-            type: "GET",
-            cache: false,
-            success: function (result, textStatus) {
-                // AJAX 下载文件的话这里会发两次请求，用 sc 来判断是否是文件，如果没有 sc 说明文件可以下载（实际上就是 result）
-                if (!result.sc) {
-                    // 再发一次请求进行正式下载
-                    window.location = latkeConfig.servePath + "/console/export/sql";
-                } else {
-                    $("#tipMsg").text(result.msg);
-                }
-            }
-        });
-    },
-    /*
-     * 获取未使用的标签。
-     * XXX: Not used this function yet.
-     */
-    getUnusedTags: function () {
-        $.ajax({
-            url: latkeConfig.servePath + "/console/tag/unused",
-            type: "GET",
-            cache: false,
-            success: function (result, textStatus) {
-                $("#tipMsg").text(result.msg);
-                if (!result.sc) {
-                    $("#loadMsg").text("");
-                    return;
-                }
+    $.ajax({
+      url: latkeConfig.servePath + "/console/export/sql",
+      type: "GET",
+      cache: false,
+      success: function (result, textStatus) {
+        // AJAX 下载文件的话这里会发两次请求，用 sc 来判断是否是文件，如果没有 sc 说明文件可以下载（实际上就是 result）
+        if (!result.sc) {
+          // 再发一次请求进行正式下载
+          window.location = latkeConfig.servePath + "/console/export/sql";
+        } else {
+          $("#tipMsg").text(result.msg);
+        }
+      }
+    });
+  },
+  /*
+ * @description 导出数据为 JSON 文件
+ */
+  exportJSON: function () {
+    $("#tipMsg").text("");
 
-                var unusedTags = result.unusedTags;
-                if (0 === unusedTags.length) {
-                    return;
-                }
-            }
-        });
-    },
-    /*
-     * @description 跟新回复提醒邮件模版
-     */
-    update: function () {
-        $("#loadMsg").text(Label.loadingLabel);
-        $("#tipMsg").text("");
+    $.ajax({
+      url: latkeConfig.servePath + "/console/export/json",
+      type: "GET",
+      cache: false,
+      success: function (result, textStatus) {
+        // AJAX 下载文件的话这里会发两次请求，用 sc 来判断是否是文件，如果没有 sc 说明文件可以下载（实际上就是 result）
+        if (!result.sc) {
+          // 再发一次请求进行正式下载
+          window.location = latkeConfig.servePath + "/console/export/json";
+        } else {
+          $("#tipMsg").text(result.msg);
+        }
+      }
+    });
+  },
+  /*
+   * 获取未使用的标签。
+   * XXX: Not used this function yet.
+   */
+  getUnusedTags: function () {
+    $.ajax({
+      url: latkeConfig.servePath + "/console/tag/unused",
+      type: "GET",
+      cache: false,
+      success: function (result, textStatus) {
+        $("#tipMsg").text(result.msg);
+        if (!result.sc) {
+          $("#loadMsg").text("");
+          return;
+        }
 
-        var requestJSONObject = {
-            "replyNotificationTemplate": {
-                "subject": $("#replayEmailTemplateTitle").val(),
-                "body": $("#replayEmailTemplateBody").val()
-            }
-        };
+        var unusedTags = result.unusedTags;
+        if (0 === unusedTags.length) {
+          return;
+        }
+      }
+    });
+  },
+  /*
+   * @description 跟新回复提醒邮件模版
+   */
+  update: function () {
+    $("#loadMsg").text(Label.loadingLabel);
+    $("#tipMsg").text("");
 
-        $.ajax({
-            url: latkeConfig.servePath + "/console/reply/notification/template",
-            type: "PUT",
-            cache: false,
-            data: JSON.stringify(requestJSONObject),
-            success: function (result, textStatus) {
-                $("#tipMsg").text(result.msg);
-                $("#loadMsg").text("");
-            }
-        });
-    }
+    var requestJSONObject = {
+      "replyNotificationTemplate": {
+        "subject": $("#replayEmailTemplateTitle").val(),
+        "body": $("#replayEmailTemplateBody").val()
+      }
+    };
+
+    $.ajax({
+      url: latkeConfig.servePath + "/console/reply/notification/template",
+      type: "PUT",
+      cache: false,
+      data: JSON.stringify(requestJSONObject),
+      success: function (result, textStatus) {
+        $("#tipMsg").text(result.msg);
+        $("#loadMsg").text("");
+      }
+    });
+  }
 };
 
 /*
  * 注册到 admin 进行管理 
  */
 admin.register.others = {
-    "obj": admin.others,
-    "init": admin.others.init,
-    "refresh": function () {
-        admin.clearTip();
-    }
+  "obj": admin.others,
+  "init": admin.others.init,
+  "refresh": function () {
+    admin.clearTip();
+  }
 };
 /*
  * Copyright (c) 2010-2017, b3log.org & hacpai.com
