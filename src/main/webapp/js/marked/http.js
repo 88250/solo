8f64f43119d54c700b2c4d14b7a0215284aa31e6
Liang Ding <dl88250@gmail.com>
2018-01-01 12:22:43 +0800

    :page_facing_up: 2018 Happy new year!

--- src/main/webapp/js/marked/http.js
+++ src/main/webapp/js/marked/http.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2017, b3log.org & hacpai.com
+ * Copyright (c) 2010-2018, b3log.org & hacpai.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * @fileoverview marked HTTP server.
  *
