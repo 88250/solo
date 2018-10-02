d5492bf9894d5ff79ed91f70587539eb6430ef9c
Van <lly219@gmail.com>
2018-10-02 19:15:29 +0800

    :arrow_up: fixed #12534

--- gulpfile.js
+++ gulpfile.js
@@ -21,38 +21,32 @@
  *
  * @author <a href="http://vanessa.b3log.org">Liyuan Li</a>
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.6.1.0, Sep 24, 2018
+ * @version 1.7.0.0, Oct 2, 2018
  */
 
 'use strict'
-
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
-const cleanCSS = require('gulp-clean-css')
-const clean = require('gulp-clean')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
 const minifycss = require('gulp-minify-css')
-const gulpSequence = require('gulp-sequence');
+const del = require('del')
 
-gulp.task('watch', function () {
-  gulp.watch('./src/main/webapp/skins/*/css/*.scss', ['sass'])
-})
-
-gulp.task('sass', function () {
+function sassProcess () {
   return gulp.src('./src/main/webapp/skins/*/css/*.scss').
     pipe(sass().on('error', sass.logError)).
     pipe(gulp.dest('./src/main/webapp/skins/'))
-})
+}
 
-gulp.task('compress', function () {
-  // min css
-  gulp.src('./src/main/webapp/js/lib/CodeMirrorEditor/codemirror.css').
-    pipe(cleanCSS()).
-    pipe(concat('codemirror.min.css')).
-    pipe(gulp.dest('./src/main/webapp/js/lib/CodeMirrorEditor/'))
+function sassProcessWatch () {
+  gulp.watch('./src/main/webapp/skins/*/css/*.scss', sassProcess)
+}
 
+gulp.task('watch', gulp.series(sassProcessWatch))
+
+
+function miniAdmin () {
   // concat js
   const jsJqueryUpload = [
     './src/main/webapp/js/lib/jquery/jquery.min.js',
@@ -67,42 +61,48 @@ gulp.task('compress', function () {
     './src/main/webapp/js/lib/CodeMirrorEditor/editor.js',
     './src/main/webapp/js/lib/to-markdown.js',
     './src/main/webapp/js/lib/highlight.js-9.6.0/highlight.pack.js']
-  gulp.src(jsJqueryUpload).
-    pipe(uglify({output: {ascii_only:true}})). // https://github.com/b3log/solo/issues/12522
+  return gulp.src(jsJqueryUpload).
+    pipe(uglify({output: {ascii_only: true}})).
+    // https://github.com/b3log/solo/issues/12522
     pipe(concat('admin-lib.min.js')).
     pipe(gulp.dest('./src/main/webapp/js/lib/compress/'))
 
+}
+
+function miniPjax (){
   // concat js
   const jsPjax = [
     './src/main/webapp/js/lib/jquery/jquery-3.1.0.min.js',
     './src/main/webapp/js/lib/jquery/jquery.pjax.js',
     './src/main/webapp/js/lib/nprogress/nprogress.js']
-  gulp.src(jsPjax).
+  return gulp.src(jsPjax).
     pipe(uglify()).
     pipe(concat('pjax.min.js')).
     pipe(gulp.dest('./src/main/webapp/js/lib/compress/'))
-})
+}
 
-gulp.task('build', function () {
+function scripts () {
+  // minify js
+  return gulp.src('./src/main/webapp/skins/*/js/*.js').
+    pipe(rename({suffix: '.min'})).
+    pipe(uglify()).
+    pipe(gulp.dest('./src/main/webapp/skins/'))
+}
+
+function styles () {
   // minify css
-  gulp.src('./src/main/webapp/skins/*/css/*.css').
+  return gulp.src('./src/main/webapp/skins/*/css/*.css').
     pipe(rename({suffix: '.min'})).
     pipe(minifycss()).
     pipe(gulp.dest('./src/main/webapp/skins/'))
+}
 
-  // minify js
-  gulp.src('./src/main/webapp/skins/*/js/*.js').
-    pipe(rename({suffix: '.min'})).
-    pipe(uglify({preserveComments: 'license'})).
-    pipe(gulp.dest('./src/main/webapp/skins/'))
-})
+function cleanProcess () {
+  return del([
+    './src/main/webapp/skins/*/css/*.min.css',
+    './src/main/webapp/skins/*/js/*.min.js'])
+}
 
-gulp.task ('clean', function () {
-  // clean css
-  gulp.src('./src/main/webapp/skins/*/css/*.min.css').pipe(clean({force: true}));
-  // clean js
-  gulp.src('./src/main/webapp/skins/*/js/*.min.js').pipe(clean({force: true}));
-});
-
-
-gulp.task('default',  gulpSequence('sass', 'build', 'compress'))
+gulp.task('default',
+  gulp.series(cleanProcess, sassProcess, gulp.parallel(scripts, styles),
+    gulp.parallel(miniPjax, miniAdmin)))
