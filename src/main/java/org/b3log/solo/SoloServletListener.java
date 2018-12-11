b303926ae2c3776fb2f3798351eea2049d4b27fe
Liang Ding <d@b3log.org>
2018-12-11 09:54:27 +0800

    :bookmark: 发布 v2.9.7

--- src/main/java/org/b3log/solo/SoloServletListener.java
+++ src/main/java/org/b3log/solo/SoloServletListener.java
@@ -66,7 +66,7 @@ public final class SoloServletListener extends AbstractServletListener {
     /**
      * Solo version.
      */
-    public static final String VERSION = "2.9.6";
+    public static final String VERSION = "2.9.7";
 
     /**
      * Bean manager.
