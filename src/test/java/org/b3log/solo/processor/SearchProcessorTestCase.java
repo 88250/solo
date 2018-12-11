6353fcffac5723b3a6b5852136260bfdbc11123f
Liang Ding <d@b3log.org>
2018-12-11 21:08:53 +0800

    :white_check_mark: #12593

---
+++ src/test/java/org/b3log/solo/processor/SearchProcessorTestCase.java
@@ -0,0 +1,76 @@
+/*
+ * Solo - A small and beautiful blogging system written in Java.
+ * Copyright (c) 2010-2018, b3log.org & hacpai.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.b3log.solo.processor;
+
+import org.apache.commons.lang.StringUtils;
+import org.b3log.solo.AbstractTestCase;
+import org.b3log.solo.MockHttpServletRequest;
+import org.b3log.solo.MockHttpServletResponse;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * {@link SearchProcessor} test case.
+ *
+ * @author <a href="http://88250.b3log.org">Liang Ding</a>
+ * @version 1.0.0.0, Dec 11, 2018
+ * @since 2.9.8
+ */
+@Test(suiteName = "processor")
+public class SearchProcessorTestCase extends AbstractTestCase {
+
+    /**
+     * Init.
+     *
+     * @throws Exception exception
+     */
+    @Test
+    public void init() throws Exception {
+        super.init();
+    }
+
+    /**
+     * showOpensearchXML.
+     */
+    @Test(dependsOnMethods = "init")
+    public void showOpensearchXML() {
+        final MockHttpServletRequest request = mockRequest();
+        request.setRequestURI("/opensearch.xml");
+        final MockHttpServletResponse response = mockResponse();
+        mockDispatcherServletService(request, response);
+
+        final String content = response.body();
+        Assert.assertTrue(StringUtils.contains(content, "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"));
+    }
+
+    /**
+     * search.
+     */
+    @Test(dependsOnMethods = "init")
+    public void search() {
+        final MockHttpServletRequest request = mockRequest();
+        request.setRequestURI("/search");
+
+        final MockHttpServletResponse response = mockResponse();
+
+        mockDispatcherServletService(request, response);
+
+        final String content = response.body();
+        Assert.assertTrue(StringUtils.contains(content, "Admin 的个人博客搜索"));
+    }
+}
