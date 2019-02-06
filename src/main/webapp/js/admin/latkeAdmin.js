4403e8916058da891920935377c303be6da885bb
Liang Ding <d@b3log.org>
2019-02-07 01:05:42 +0800

    :package: 前端资源打包

--- src/main/webapp/js/admin/latkeAdmin.js
+++ src/main/webapp/js/admin/latkeAdmin.js
@@ -3150,7 +3150,6 @@ admin.userList = {
                 "userName": $("#userName").val(),
                 "userEmail": $("#userEmail").val(),
                 "userURL": $("#userURL").val(),
-                "userPassword": $("#userPassword").val(),
                 "userAvatar": $("#userAvatar").val()
             };
 
@@ -3169,7 +3168,6 @@ admin.userList = {
                     $("#userName").val("");
                     $("#userEmail").val("");
                     $("#userURL").val("");
-                    $("#userPassword").val("");
                     $("#userAvatar").val("");
                     if (admin.userList.pageInfo.currentCount === Label.PAGE_SIZE &&
                             admin.userList.pageInfo.currentPage === admin.userList.pageInfo.pageCount) {
@@ -3214,7 +3212,6 @@ admin.userList = {
                 });
                 $userEmailUpdate.val(result.user.userEmail);
                 $("#userURLUpdate").val(result.user.userURL);
-                $("#userPasswordUpdate").val(result.user.userPassword);
                 $("#userAvatarUpdate").val(result.user.userAvatar);
 
                 $("#loadMsg").text("");
@@ -3236,7 +3233,6 @@ admin.userList = {
                 "userEmail": $("#userEmailUpdate").val(),
                 "userURL": $("#userURLUpdate").val(),
                 "userRole": userInfo.userRole,
-                "userPassword": $("#userPasswordUpdate").val(),
                 "userAvatar": $("#userAvatarUpdate").val()
             };
 
@@ -3350,9 +3346,6 @@ admin.userList = {
         } else if (!/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test($("#userEmail" + status).val())) {
             $("#tipMsg").text(Label.mailInvalidLabel);
             $("#userEmail" + status).focus();
-        } else if ($("#userPassword" + status).val() === "") {
-            $("#tipMsg").text(Label.passwordEmptyLabel);
-            $("#userPassword" + status).focus();
         } else {
             return true;
         }
