ebd1e57d1080d3b598886a9b8445f804b61ec447
Liang Ding <d@b3log.org>
2019-02-28 11:23:57 +0800

    :art: 切换默认皮肤为 Pinghsu

--- src/main/java/org/b3log/solo/model/Option.java
+++ src/main/java/org/b3log/solo/model/Option.java
@@ -337,7 +337,7 @@ public final class Option {
         /**
          * Default skin directory name.
          */
-        public static final String DEFAULT_SKIN_DIR_NAME = "Jane";
+        public static final String DEFAULT_SKIN_DIR_NAME = "Pinghsu";
 
         /**
          * Default language.
