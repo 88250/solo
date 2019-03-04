536156fe1198a04a21d1b84b01c86c9123460835
Liang Ding <d@b3log.org>
2019-03-04 12:17:07 +0800

    :art: #12688

--- src/main/java/org/b3log/solo/processor/KanBanNiangProcessor.java
+++ src/main/java/org/b3log/solo/processor/KanBanNiangProcessor.java
@@ -27,15 +27,17 @@ import org.b3log.latke.servlet.annotation.RequestProcessing;
 import org.b3log.latke.servlet.annotation.RequestProcessor;
 import org.b3log.latke.servlet.renderer.JsonRenderer;
 import org.b3log.solo.SoloServletListener;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.servlet.ServletContext;
 import java.io.InputStream;
 
 /**
  * KanBanNiang processor. https://github.com/b3log/solo/issues/12472
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.0.1, Dec 3, 2018
+ * @version 1.0.0.2, Mar 4, 2019
  * @since 2.9.2
  */
 @RequestProcessor
@@ -56,73 +58,33 @@ public class KanBanNiangProcessor {
         final JsonRenderer renderer = new JsonRenderer();
         context.setRenderer(renderer);
         try {
-            final int i = RandomUtils.nextInt(MODEL_NAMES.length);
-            final String name = MODEL_NAMES[i];
-            final InputStream modelResource = SoloServletListener.getServletContext().getResourceAsStream("/plugins/kanbanniang/assert/model/bilibili-live/22/index.json");
-            String s = IOUtils.toString(modelResource, "UTF-8");
-            s = s.replace("${name}", name);
-            final JSONObject model = new JSONObject(s);
-            renderer.setJSONObject(model);
+            final String assets = "/plugins/kanbanniang/assets";
+            String model;
+            final ServletContext servletContext = SoloServletListener.getServletContext();
+            try (final InputStream inputStream = servletContext.getResourceAsStream(assets + "/model-list.json")) {
+                final JSONArray models = new JSONArray(IOUtils.toString(inputStream, "UTF-8"));
+                final int i = RandomUtils.nextInt(models.length());
+                model = models.getString(i);
+            }
+
+            try (final InputStream modelResource = servletContext.getResourceAsStream(assets + "/model/" + model + "/index.json")) {
+                final JSONObject index = new JSONObject(IOUtils.toString(modelResource, "UTF-8"));
+                final JSONArray textures = index.optJSONArray("textures");
+                if (textures.isEmpty()) {
+                    try (final InputStream texturesRes = servletContext.getResourceAsStream(assets + "/model/" + model + "/textures.json")) {
+                        final JSONArray texturesArray = new JSONArray(IOUtils.toString(texturesRes, "UTF-8"));
+                        final Object element = texturesArray.opt(RandomUtils.nextInt(texturesArray.length()));
+                        if (element instanceof JSONArray) {
+                            index.put("textures", element);
+                        } else {
+                            index.put("textures", new JSONArray().put(element));
+                        }
+                    }
+                }
+                renderer.setJSONObject(index);
+            }
         } catch (final Exception e) {
             LOGGER.log(Level.ERROR, "Returns a random KanBanNiang model failed", e);
         }
     }
-
-    /**
-     * Model names.
-     */
-    private static final String[] MODEL_NAMES = {
-            "Animal Costume.png",
-            "Animal Costume Racoon.png",
-            "Bunny Girl Costume.png",
-            "Bunny Girl Costume Red.png",
-            "Cake Costume Choco.png",
-            "Cake Costume Cream.png",
-            "default-costume.png",
-            "Dress Costume.png",
-            "Dress Costume Brown.png",
-            "Elementary School Costume.png",
-            "Elementary School Costume Navy.png",
-            "Frill Bikini Costume Green.png",
-            "Furisode Costume.png",
-            "Halloween Costume.png",
-            "Hanbok Costume.png",
-            "Kids Costume.png",
-            "Kids Costume Navy.png",
-            "Maid Costume.png",
-            "Maid Costume Red.png",
-            "New2015 Costume.png",
-            "New2015 Costume Pajamas.png",
-            "Nightsky Costume.png",
-            "pajamas-costume.png",
-            "Pajamas Costume Pink.png",
-            "Qipao Costume Pink.png",
-            "Qipao Costume Red.png",
-            "Sailor Costume.png",
-            "Sailor Costume Black.png",
-            "Sakura Costume.png",
-            "Sakura Costume Navy.png",
-            "Santa Costume.png",
-            "Santa Costume Green.png",
-            "Sarori Costume.png",
-            "school-costume.png",
-            "School Costume Red.png",
-            "Star Witch Costume.png",
-            "Star Witch Costume Brown.png",
-            "Succubus Costume Black.png",
-            "Succubus Costume Red.png",
-            "Sukumizu Costume.png",
-            "Sukumizu Costume White.png",
-            "Summer Dress Costume Blue.png",
-            "Summer Dress Costume White.png",
-            "Tirami1 Costume.png",
-            "Turtleneck Costume.png",
-            "Turtleneck Costume Red.png",
-            "Winter Coat Costume Pink.png",
-            "Winter Coat Costume White.png",
-            "Winter Costume.png",
-            "Winter Costume White.png",
-            "Witch Costume.png",
-            "Witch Costume White.png"
-    };
 }
