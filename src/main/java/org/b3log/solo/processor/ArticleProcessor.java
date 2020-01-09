6d396afb0ad71bb5d0a53dd54ec158c59e62b665
Liang Ding <d@b3log.org>
2020-01-09 10:12:01 +0800

    :sparkles: #19 随机、相关文章处理

--- src/main/java/org/b3log/solo/processor/ArticleProcessor.java
+++ src/main/java/org/b3log/solo/processor/ArticleProcessor.java
@@ -55,7 +55,7 @@ import java.util.*;
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author <a href="http://zephyr.b3log.org">Zephyr</a>
- * @version 1.4.5.8, Sep 11, 2019
+ * @version 1.4.5.9, Jan 9, 2020
  * @since 0.3.1
  */
 @RequestProcessor
@@ -957,7 +957,9 @@ public class ArticleProcessor {
         Stopwatchs.end();
 
         dataModel.put(Option.ID_C_EXTERNAL_RELEVANT_ARTICLES_DISPLAY_CNT, preference.getInt(Option.ID_C_EXTERNAL_RELEVANT_ARTICLES_DISPLAY_CNT));
-        dataModel.put(Option.ID_C_RANDOM_ARTICLES_DISPLAY_CNT, preference.getInt(Option.ID_C_RANDOM_ARTICLES_DISPLAY_CNT));
-        dataModel.put(Option.ID_C_RELEVANT_ARTICLES_DISPLAY_CNT, preference.getInt(Option.ID_C_RELEVANT_ARTICLES_DISPLAY_CNT));
+        if (!Solos.GEN_STATIC_SITE) {
+            dataModel.put(Option.ID_C_RANDOM_ARTICLES_DISPLAY_CNT, preference.getInt(Option.ID_C_RANDOM_ARTICLES_DISPLAY_CNT));
+            dataModel.put(Option.ID_C_RELEVANT_ARTICLES_DISPLAY_CNT, preference.getInt(Option.ID_C_RELEVANT_ARTICLES_DISPLAY_CNT));
+        }
     }
 }
