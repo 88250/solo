12d54174962b28a6930f2cd1de484f7c9a4c8d58
Van <lly219@gmail.com>
2020-01-25 21:47:58 +0800

    :arrow_up: vditor

--- src/main/resources/js/common.js
+++ src/main/resources/js/common.js
@@ -307,7 +307,7 @@ window.Util = {
 (() => {
   $.ajax({
     method: 'GET',
-    url: 'https://cdn.jsdelivr.net/npm/vditor@2.1.2/dist/index.min.js',
+    url: 'https://cdn.jsdelivr.net/npm/vditor@2.1.4/dist/index.min.js',
     dataType: 'script',
     cache: true,
     success: () => {
