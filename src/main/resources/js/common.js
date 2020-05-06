80c5d7b1adadc6b5ad4bc32fcd4e5575eb9c6d8e
Liang Ding <d@b3log.org>
2020-05-06 21:51:25 +0800

    :arrow_up: 升级 Vditor

--- src/main/resources/js/common.js
+++ src/main/resources/js/common.js
@@ -306,7 +306,7 @@ window.Util = {
   loadVditor: function (cb) {
     $.ajax({
       method: 'GET',
-      url: 'https://cdn.jsdelivr.net/npm/vditor@3.1.23/dist/index.min.js',
+      url: 'https://cdn.jsdelivr.net/npm/vditor@3.2.0/dist/index.min.js',
       dataType: 'script',
       cache: true,
       success: () => {
