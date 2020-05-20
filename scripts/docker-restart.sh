a1fdca3e4eaa5b20a15ee088e211bcc40b15d4ab
Liang Ding <d@b3log.org>
2020-05-20 10:52:15 +0800

    :art: 稍微做了一些调整 @vcjmhg

--- scripts/docker-restart.sh
+++ scripts/docker-restart.sh
@@ -10,8 +10,6 @@
 restart_solo(){
 	docker stop solo
 	docker rm solo
-	docker stop solo
-	docker rm solo
 	docker run --detach --name solo --network=host \
 	--env RUNTIME_DB="MYSQL" \
 	--env JDBC_USERNAME="root" \
@@ -22,29 +20,29 @@ restart_solo(){
 }
 
 update_solo(){
-	isUpdate=$(docker pull b3log/solo|grep "Downloaded")
-
-	if [[ -z  $isUpdate ]]
+	isUpdate=$(docker pull b3log/solo | grep "Downloaded")
+	if [[ -z $isUpdate ]]
 	then
-		echo -e "\033[32m 当前solo为最新版本. \033[0m"
+		echo "Solo is up to date"
 	else
 		restart_solo >> /dev/null 2>&1
-		echo -e "\033[32m solo部署成功!!\033[0m"
+		echo "Restarted Solo"
 	fi
 }
-#检查当前容器状态，如果状态正常进行升级操作，否则重新进行部署
+
+# 检查当前容器状态，如果状态正常进行升级操作，否则重新进行部署
 update_and_test_service(){
 	isCrash=$(docker ps | grep "b3log/solo")
 	if [[ -z $isCrash ]]
 	then
-		echo -e "\033[31m solo状态异常，正重新部署...  \033[0m"
+		echo "Solo's status is unexpected, trying to restart it"
 		docker pull b3log/solo
 		restart_solo
 		sleep 5
 		isSecondCrash=$(docker ps | grep "b3log/solo")
 		if [[ -z $isSecondCrash ]]
 		then
-			echo -e "\033[31m 重新部署失败，可能原因:solo的启动参数可能有误，请仔细检查!!  \033[0m"
+			echo "Failed to restart Solo, please check logs via 'docker logs solo'"
 		fi
 	else
 		update_solo
