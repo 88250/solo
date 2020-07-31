7703ed17f2fca567ac279a4093eadcb143623a96
Liyuan Li <v@b3log.org>
2020-07-31 23:33:17 +0800

    :arrow_up: vditor

--- src/main/resources/js/common.js
+++ src/main/resources/js/common.js
@@ -307,7 +307,7 @@ window.Util = {
   loadVditor: function (cb) {
     $.ajax({
       method: 'GET',
-      url: 'https://cdn.jsdelivr.net/npm/vditor@3.4.0/dist/index.min.js',
+      url: 'https://cdn.jsdelivr.net/npm/vditor@3.4.1/dist/index.min.js',
       dataType: 'script',
       cache: true,
       success: () => {
