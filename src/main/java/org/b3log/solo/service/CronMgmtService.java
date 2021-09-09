b0aaf827b4a6b80f3396e44ba337e944385b6463
Liang Ding <d@b3log.org>
2021-09-09 10:23:01 +0800

    :art: 获取社区黑名单 IP 改为定时一小时

--- src/main/java/org/b3log/solo/service/CronMgmtService.java
+++ src/main/java/org/b3log/solo/service/CronMgmtService.java
@@ -27,7 +27,7 @@ import java.util.concurrent.TimeUnit;
  * Cron management service.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.0.6, Jun 28, 2020
+ * @version 1.0.0.7, Sep 9, 2021
  * @since 2.9.7
  */
 @Service
@@ -99,7 +99,7 @@ public class CronMgmtService {
             } finally {
                 Stopwatchs.release();
             }
-        }, delay, 1000 * 60 * 30, TimeUnit.MILLISECONDS);
+        }, delay, 1000 * 60 * 60, TimeUnit.MILLISECONDS);
         delay += 2000;
 
         SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
